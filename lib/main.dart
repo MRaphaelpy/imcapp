5204cae84401b85b862f58cfe927fc9ba10cef24
Marcos Rafael <89277533+MRaphaelpy@users.noreply.github.com>
2021-10-11 15:31:23 -0300

    Interface

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,31 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Calculadora de IMC"),
         centerTitle: true,
+        backgroundColor: Colors.purple,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Digite seu peso em (Kg)",
+              labelStyle: TextStyle(color: Colors.purple),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.purple, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Digite sua altura em (cm)",
+              labelStyle: TextStyle(color: Colors.purple, fontSize: 25.0),
+            ),
+          )
+        ],
       ),
     );
   }

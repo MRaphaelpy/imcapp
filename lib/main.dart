6f29963874628233183650150c8cae6523399bf5
Marcos Rafael <89277533+MRaphaelpy@users.noreply.github.com>
2021-10-15 12:56:46 -0300

    Faixas de peso Adicionadas

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,19 @@ class _HomeState extends State<Home> {
       double imc = peso / (altura * altura);
       if (imc < 18.6) {
         _result = "(${imc.toStringAsPrecision(3)}) Voce esta Abaixo do peso :(";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _result =
+            "(${imc.toStringAsPrecision(3)}) Voce esta No peso Ideal XD :(";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _result =
+            "(${imc.toStringAsPrecision(3)}) Voce esta Levemente Acima do Peso :O";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _result = "(${imc.toStringAsPrecision(3)}) Obesidade Nivel I ";
+      } else if (imc >= 39.9 && imc < 39.9) {
+        _result = "(${imc.toStringAsPrecision(3)}) Obesidade Nivel II ";
+      } else if (imc >= 40) {
+        _result =
+            "(${imc.toStringAsPrecision(3)})Extremo cuido voce esta com Obesidade Nivel III ";
       }
     });
   }

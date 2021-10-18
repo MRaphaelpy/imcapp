3f3fb79632f33d3bd54ba67d8f6bcf6a15751a27
Marcos Rafael <89277533+MRaphaelpy@users.noreply.github.com>
2021-10-18 11:48:14 -0300

    LastUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,7 @@ class _HomeState extends State<Home> {
       if (imc < 18.6) {
         _result = "(${imc.toStringAsPrecision(3)}) Voce esta Abaixo do peso :(";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _result =
-            "(${imc.toStringAsPrecision(3)}) Voce esta No peso Ideal XD :(";
+        _result = "(${imc.toStringAsPrecision(3)}) Voce esta No peso Ideal XD ";
       } else if (imc >= 24.9 && imc < 29.9) {
         _result =
             "(${imc.toStringAsPrecision(3)}) Voce esta Levemente Acima do Peso :O";
@@ -95,6 +94,8 @@ class _HomeState extends State<Home> {
                       labelStyle:
                           TextStyle(color: Colors.purple, fontSize: 25.0),
                     ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.purple, fontSize: 25),
                     controller: alturaController,
                     validator: (value) {
                       if (value!.isEmpty) {
